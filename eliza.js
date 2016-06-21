378538acadeca08f3efd885699e0d2f8ef0b34db
Eric Martindale <eric@ericmartindale.com>
2016-06-21 16:43:49 -0700

    Upgrade to new snarl API.

--- eliza.js
+++ eliza.js
@@ -8,7 +8,7 @@ var initial = eliza.getInitial();
 module.exports = {
   conversation: 'You can @mention me in a channel (or DM me) and I\'ll start a conversation with you.  After you get my attention, no need to tag me again -- but I\'ll stop if you don\'t say anything for a few minutes.  I have things to do!',
   '{DM}': function(text, cb) {
-    var reply = eliza.transform(text);
+    var reply = eliza.transform(text.parsed);
     cb(null, reply);
   }
 }
